49c12a4fb3cbfe6d0e9c024cde1ab1978df8a03e
rusted-coil <rusted.coil@gmail.com>
2025-08-08 17:21:33 +0900

    再び聖戦トラキアを有効化

--- FERNGSolver.UI/FormLauncher.cs
+++ FERNGSolver.UI/FormLauncher.cs
@@ -27,8 +27,8 @@ namespace FERNGSolver.UI
 
             // 作品個別コントロールを初期化
             var entries = new IMainFormEntry[]{
-//                Genealogy.UI.MainFormEntryProvider.Create(errorNotifier),
-//                Thracia.UI.MainFormEntryProvider.Create(errorNotifier),
+                Genealogy.UI.MainFormEntryProvider.Create(errorNotifier),
+                Thracia.UI.MainFormEntryProvider.Create(errorNotifier),
                 Gba.UI.MainFormEntryProvider.Create(gbaConfigService, errorNotifier),
             };
             form.SetEntries(entries);
