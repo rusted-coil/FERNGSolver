8a4d9350e16cfd48c78ac5960478d6b6b7a20714
rusted-coil <rusted.coil@gmail.com>
2025-08-30 18:36:07 +0900

    Configファイルが無い時に起動しなくなっていたので修正

--- FERNGSolver.Application/Config/Internal/Config.cs
+++ FERNGSolver.Application/Config/Internal/Config.cs
@@ -2,6 +2,6 @@ namespace FERNGSolver.Application.Config.Internal
 {
     internal sealed class Config : IModifiableConfig
     {
-        public string SelectingTitle { get; set; } = "GBA";
+        public string SelectingTitle { get; set; } = "聖戦の系譜";
     }
 }
