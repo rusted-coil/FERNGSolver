eeea3d57fca5868602e840e0aea062e90ea777ed
rusted-coil <rusted.coil@gmail.com>
2026-07-29 21:23:05 +0900

    Rename FERNGSolver.Web project to FERNGSolver.Radiance.Web for multi-title clarity

--- FERNGSolver.Radiance.Web/Program.cs
+++ FERNGSolver.Radiance.Web/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using FERNGSolver.Web;
+using FERNGSolver.Radiance.Web;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
